726aceaa24040f5a90f45828e167f57ddf5bae89
i3right <brightz@live.com>
2024-08-12 12:03:27 +0700

    update: complete front-end overview

--- index.js
+++ index.js
@@ -1,41 +1,17 @@
-// Declaration Variable
-
-let my_name = "DevHub";
-let age = 18;
-let isStudent = true;
-
-// console.log("My name is: :", my_name)
-// console.log("I'm study:", isStudent)
-// console.log("I'm ", age)
-
-function greet(my_name) {
-    alert("Hello "+ my_name)
-}
-
-
-let score = 85
-
-if(score >= 90){
-    console.log("Very Good!");
-} else if (score >= 80) {
-    console.log("Good!");
-} else {
-    console.log("Keep Tryings")
-}
-
-if (isStudent) {
-    console.log("Let focus");
-} else {
-    console.log("Relax then");
-}
-
-// Loop
-
-for (let i = 0; i < 5; i++) {
-    console.log("Print out: ", i);
-}
-
 // DOM 
 function changeBg() {
     document.body.style.background = "white"
+}
+
+function addText() {
+    let inputText = document.getElementById("userInput")
+    
+    let newMessage = document.createElement("p")
+    newMessage.textContent = inputText.value
+    
+    let msgBox = document.getElementById("messageBox")
+    msgBox.appendChild(newMessage)
+
+    inputText.value = ""
+
 }
